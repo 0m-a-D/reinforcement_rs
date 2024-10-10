3a55f444851f8b20ae489a293e6d8ba1d9650fbc
Om Anand <omishere21@gmail.com>
2024-10-10 21:17:50 +0530

    minor changes/refactorings

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 mod agent;
 mod environment;
+mod neural_net;
 mod utils;
 use rand::{thread_rng, Rng};
 use raylib::prelude::*;
 
-const WINDOW_WIDTH: i32 = 800;
-const WINDOW_HEIGHT: i32 = 450;
-const SNAKE_SIZE: f32 = 20.0;
+pub const WINDOW_WIDTH: i32 = 800;
+pub const WINDOW_HEIGHT: i32 = 450;
+pub const SNAKE_INIT_SIZE: f32 = 40.0;
+pub const SPEED: f32 = 0.18;
 
 pub fn app() {
     let (mut rl, thread) = raylib::init()
@@ -14,9 +16,7 @@ pub fn app() {
         .title("Moving snake head")
         .build();
 
-    let mut snake_head = Rectangle::new(400.0, 225.0, 20.0, 20.0);
-
-    let speed = 0.09;
+    let mut snake_head = Rectangle::new(400.0, 225.0, SNAKE_INIT_SIZE, SNAKE_INIT_SIZE);
 
     let mut should_quit = false;
     let mut food = generate_food();
@@ -33,16 +33,16 @@ pub fn app() {
         let delta_time = rl.get_frame_time();
 
         if rl.is_key_down(KeyboardKey::KEY_RIGHT) {
-            snake_head.x = (snake_head.x + speed).min(WINDOW_WIDTH as f32 - SNAKE_SIZE);
+            snake_head.x = (snake_head.x + SPEED).min(WINDOW_WIDTH as f32 - SNAKE_INIT_SIZE);
         }
         if rl.is_key_down(KeyboardKey::KEY_LEFT) {
-            snake_head.x = (snake_head.x - speed).max(0.0);
+            snake_head.x = (snake_head.x - SPEED).max(0.0);
         }
         if rl.is_key_down(KeyboardKey::KEY_UP) {
-            snake_head.y = (snake_head.y - speed).max(0.0);
+            snake_head.y = (snake_head.y - SPEED).max(0.0);
         }
         if rl.is_key_down(KeyboardKey::KEY_DOWN) {
-            snake_head.y = (snake_head.y + speed).min(WINDOW_HEIGHT as f32 - SNAKE_SIZE);
+            snake_head.y = (snake_head.y + SPEED).min(WINDOW_HEIGHT as f32 - SNAKE_INIT_SIZE);
         }
 
         let mut d = rl.begin_drawing(&thread);
@@ -52,9 +52,9 @@ pub fn app() {
             food_timer = 0.0;
         }
 
-        d.clear_background(Color::RAYWHITE);
-        d.draw_rectangle_rec(snake_head, Color::GREEN);
-        d.draw_rectangle_rec(food, Color::BLACK);
+        d.clear_background(Color::BLACK);
+        d.draw_rectangle_rec(snake_head, Color::BLUE);
+        d.draw_rectangle_rec(food, Color::RED);
         d.draw_text("Use arrow keys to move", 20, 20, 20, Color::GRAY);
 
         if d.gui_button(rect, Some(rstr!("Quit"))) {
@@ -67,7 +67,7 @@ fn generate_food() -> Rectangle {
     Rectangle::new(
         thread_rng().gen_range(0.0..800.0),
         thread_rng().gen_range(0.0..450.0),
-        10.0,
-        10.0,
+        7.0,
+        7.0,
     )
 }
